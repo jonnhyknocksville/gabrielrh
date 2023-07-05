3bfd298611a40fff39847be069fc41956514c244
vermin-le-germin <122983999+Antoine-developpeur@users.noreply.github.com>
2023-07-05 15:10:57 +0200

    create form page-create formController-create Entity former-create formerForm and add data to database

---
+++ src/Controller/FormerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Former;
+use App\Form\FormerType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class FormerController extends AbstractController
+{
+    #[Route('/formateur', name: 'app_former')]
+    public function index(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
+    {
+        $former = new Former();
+        $form = $this->createForm(FormerType::class, $former);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $former = $form->getData();
+            $entityManager->persist($former);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_former');
+        }
+
+        return $this->render('former/index.html.twig', [
+            'former_form' => $form,
+        ]);
+    }
+}
---
+++ src/Form/FormerType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Former;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,array('label'=>'Nom'))
+            ->add('lastName',TextType::class,array('label'=>'Prénom'))
+            ->add('Email', EmailType::class,array('label'=>'E-mail'))
+            ->add('phone',NumberType::class,array('label'=>'Téléphone'))
+            ->add('adress', TextType::class,array('label'=>'Adresse'))
+            ->add('city', TextType::class,array('label'=>'Ville'))
+            ->add('purpose' ,ChoiceType::class, [ 'label'=>'Motif',
+                'choices'  => [
+                    'Je cherche un poste de formateur en ligne' => 'formateur en ligne',
+                    'Je cherche un poste de fromateur en présentiel' => 'formateur presentiel',
+                    'Je veux devenir formateur' => 'devenir formateur',
+                    'Autre' => 'autre',
+                ],
+            ])
+            ->add('CV', FileType::class)
+            ->add('message',TextType::class,array('label'=>'Méssage'))
+            ->add('Envoyer', SubmitType::class,)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Former::class,
+        ]);
+    }
+}
