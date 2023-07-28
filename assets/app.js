21a9a41d9e03007ee76647cee83bb78a7ab89f3b
vermin-le-germin <122983999+Antoine-developpeur@users.noreply.github.com>
2023-07-28 11:31:55 +0200

    last version

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-console.log("teste");
 
 document.addEventListener('DOMContentLoaded', function() {
     const TButton = document.getElementById('fromButton');
